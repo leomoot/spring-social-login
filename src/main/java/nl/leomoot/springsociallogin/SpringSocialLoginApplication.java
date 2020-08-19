7d169055460d15b1aebb3453b2cea5caedfa2078
Leo Moot <leo.moot@gmail.com>
2020-08-19 13:23:55 +0200

    Add support for ApplicationProperties

--- src/main/java/nl/leomoot/springsociallogin/SpringSocialLoginApplication.java
+++ src/main/java/nl/leomoot/springsociallogin/SpringSocialLoginApplication.java
@@ -2,12 +2,15 @@ package nl.leomoot.springsociallogin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import nl.leomoot.springsociallogin.config.ApplicationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(ApplicationProperties.class)
 public class SpringSocialLoginApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringSocialLoginApplication.class, args);
-	}
+  public static void main(final String[] args) {
+    SpringApplication.run(SpringSocialLoginApplication.class, args);
+  }
 
 }
--- src/main/java/nl/leomoot/springsociallogin/config/ApplicationProperties.java
+++ src/main/java/nl/leomoot/springsociallogin/config/ApplicationProperties.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-
 @ConfigurationProperties(prefix = "application")
 public class ApplicationProperties {
 
