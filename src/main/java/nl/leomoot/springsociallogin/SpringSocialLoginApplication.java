b12ff1075e9f2706c7227517b52fdb6d403c5935
Leo Moot <leo.moot@gmail.com>
2020-08-19 10:56:02 +0200

    initial setup

---
+++ src/main/java/nl/leomoot/springsociallogin/SpringSocialLoginApplication.java
@@ -0,0 +1,13 @@
+package nl.leomoot.springsociallogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSocialLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSocialLoginApplication.class, args);
+	}
+
+}
