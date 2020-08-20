d9216c638f132bd42f51454bf5942f026d0aceb2
Leo Moot <leo.moot@gmail.com>
2020-08-20 16:09:40 +0200

    oauth2 login

--- src/main/java/nl/leomoot/springsociallogin/config/SecurityConfig.java
+++ src/main/java/nl/leomoot/springsociallogin/config/SecurityConfig.java
@@ -1,30 +1,39 @@
 package nl.leomoot.springsociallogin.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import nl.leomoot.springsociallogin.security.oauth2.CustomOAuth2UserService;
+import nl.leomoot.springsociallogin.security.oauth2.CustomOidcUserService;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
- 
+     
     private final OidcUserService oidcUserService;
+    private final OAuth2UserService<OAuth2UserRequest, OAuth2User> oauth2UserService;
     
-    public SecurityConfig(final OidcUserService oidcUserService) {
+    public SecurityConfig(final CustomOAuth2UserService oauth2UserService, final CustomOidcUserService oidcUserService) {
         this.oidcUserService = oidcUserService;
+        this.oauth2UserService = oauth2UserService;
     }
     
     @Override
-    protected void configure(HttpSecurity http) throws Exception {        
+    protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
             .anyRequest().authenticated()
             .and()
-            .csrf(c -> c.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+            .httpBasic(b -> b.disable())
+            .csrf(c -> c.disable())
             .oauth2Login()
-            .defaultSuccessUrl("/oauth2LoginSuccess")
-            .userInfoEndpoint(u -> u.oidcUserService(oidcUserService)); // OpenID
+     //       .defaultSuccessUrl("/oauth2LoginSuccess")
+            .userInfoEndpoint(u -> u.userService(oauth2UserService))
+            .userInfoEndpoint(u -> u.oidcUserService(oidcUserService));
     }
 }
--- src/main/java/nl/leomoot/springsociallogin/controller/LoginController.java
+++ src/main/java/nl/leomoot/springsociallogin/controller/LoginController.java
@@ -4,6 +4,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,12 +14,10 @@ public class LoginController {
    
     @GetMapping("/oauth2LoginSuccess")
     public ResponseEntity<String> checklogin(OAuth2AuthenticationToken authentication) {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        if (auth instanceof OAuth2AuthenticationToken) {
-            OAuth2AuthenticationToken oauth2 = (OAuth2AuthenticationToken) auth;
-            Object details = auth.getDetails();
-          
-        }
-        return ResponseEntity.ok(authentication.getAuthorizedClientRegistrationId());
+        OAuth2User user = authentication.getPrincipal();
+        String name = user.getAttribute("name");
+       
+        return ResponseEntity.ok(String.format("Hello %s, you were authenticated using %s -> %s", name, 
+                authentication.getAuthorizedClientRegistrationId(), user));
     }
 }
