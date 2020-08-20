c3f15437f70e362a93ce790dbaee71de4a323cfa
Leo Moot <leo.moot@gmail.com>
2020-08-20 16:30:07 +0200

    changed SecurityConfig

--- src/main/java/nl/leomoot/springsociallogin/config/SecurityConfig.java
+++ src/main/java/nl/leomoot/springsociallogin/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package nl.leomoot.springsociallogin.config;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -9,6 +8,7 @@ import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import nl.leomoot.springsociallogin.security.oauth2.CustomOAuth2UserService;
 import nl.leomoot.springsociallogin.security.oauth2.CustomOidcUserService;
 
@@ -30,9 +30,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .anyRequest().authenticated()
             .and()
             .httpBasic(b -> b.disable())
-            .csrf(c -> c.disable())
+            .csrf(c -> c.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
             .oauth2Login()
-     //       .defaultSuccessUrl("/oauth2LoginSuccess")
             .userInfoEndpoint(u -> u.userService(oauth2UserService))
             .userInfoEndpoint(u -> u.oidcUserService(oidcUserService));
     }
