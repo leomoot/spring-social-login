a18e8b901a27f7a80923d89c9e2289bf7df569f1
Leo Moot <leo.moot@gmail.com>
2020-08-21 15:17:02 +0200

    removed shared functionality

--- src/main/java/nl/leomoot/springsociallogin/config/SecurityConfig.java
+++ src/main/java/nl/leomoot/springsociallogin/config/SecurityConfig.java
@@ -5,18 +5,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import lombok.RequiredArgsConstructor;
-import nl.leomoot.springsociallogin.security.oauth2.CustomOAuth2UserService;
-import nl.leomoot.springsociallogin.security.oauth2.CustomOidcUserService;
 
-@RequiredArgsConstructor
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-     
-    private final CustomOidcUserService customOidcUserService;
-    private final CustomOAuth2UserService customOAuth2UserService;
-    
+         
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
@@ -24,8 +17,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .and()
             .httpBasic(b -> b.disable())
             .csrf(c -> c.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
-            .oauth2Login()
-            .userInfoEndpoint(u -> u.userService(customOAuth2UserService))
-            .userInfoEndpoint(u -> u.oidcUserService(customOidcUserService)); // Open ID
-    }
+            .oauth2Login();
+   }
 }
