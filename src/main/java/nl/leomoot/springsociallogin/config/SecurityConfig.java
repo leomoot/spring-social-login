f14055457d0db9bc21133c0a9fb69aa7a9b83d51
Leo Moot <leo.moot@gmail.com>
2020-08-25 15:57:05 +0200

    rewrote security config

--- src/main/java/nl/leomoot/springsociallogin/config/SecurityConfig.java
+++ src/main/java/nl/leomoot/springsociallogin/config/SecurityConfig.java
@@ -1,37 +1,55 @@
 package nl.leomoot.springsociallogin.config;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import lombok.RequiredArgsConstructor;
 import nl.leomoot.springsociallogin.security.oauth2.CustomOAuth2UserService;
 import nl.leomoot.springsociallogin.security.oauth2.CustomOidcUserService;
-import nl.leomoot.springsociallogin.security.oauth2.OAuth2AuthenticationSuccessHandler;
 
 @RequiredArgsConstructor
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(securedEnabled = true, jsr250Enabled = true, prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-     
+ 
     private final CustomOidcUserService customOidcUserService;
     private final CustomOAuth2UserService customOAuth2UserService;
-    private final OAuth2AuthenticationSuccessHandler successHandler;
-    
+ 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-            .anyRequest().authenticated()
-            .and()
-            .httpBasic(b -> b.disable())
-            .csrf(c -> c.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
-            .oauth2Login()
-            .userInfoEndpoint(u -> u.userService(customOAuth2UserService))
-            .userInfoEndpoint(u -> u.oidcUserService(customOidcUserService)) // Open ID
-            .successHandler(successHandler)
-       //     .failureHandler(authenticationFailureHandler)
-            ;
-            
+        // @formatter:off
+        http
+            .cors(c -> c
+                    .and()
+            )
+            .sessionManagement(s -> s
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            )    
+            .csrf(c -> c
+                    .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+            )
+            .formLogin(f -> f
+                    .disable()
+            )
+            .httpBasic(h -> h
+                    .disable()
+            )            
+            .authorizeRequests(a -> a
+                    .antMatchers("/", "/error", "/webjars/**").permitAll()
+                    .anyRequest().authenticated()
+            )
+            .exceptionHandling(e -> e
+                    .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+            )        
+            .oauth2Login(o -> o
+                    .userInfoEndpoint(u -> u
+                            .userService(customOAuth2UserService)
+                            .oidcUserService(customOidcUserService)
+                    )
+            );
+         // @formatter:on
     }
 }
